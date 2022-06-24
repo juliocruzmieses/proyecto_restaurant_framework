7011fcfc9c5519154e55a9fb8d9c90ec4787d5f8
PatriciaLavi <PatriciaLavi@gmail.com>
2022-06-24 14:50:54 -0500

    ultimoavance

--- Proyecto_Restaurant/Models/MesaModel.cs
+++ Proyecto_Restaurant/Models/MesaModel.cs
@@ -10,6 +10,10 @@ namespace Proyecto_Restaurant.Models
     {
         [Display(Name = "Código de Mesa")]
         public string idMesa { get; set; }
+
+        [Required(ErrorMessage = "Este campo es obligatorio")]
+        [MinLength(6, ErrorMessage = "Debe contener minimo 6 caracteres")]
+
         [Display(Name = "N° de Mesa")]
         public string descMesa { get; set; }
     }
--- Proyecto_Restaurant/Models/UsuarioModel.cs
+++ Proyecto_Restaurant/Models/UsuarioModel.cs
@@ -11,22 +11,45 @@ namespace Proyecto_Restaurant.Models
         [Display(Name = "Código de Usuario")]
         public int id_usuario { get; set; }
         [Display(Name = "Nombre de Usuario")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El nombre es obligatorio.")]
+        [MinLength(3, ErrorMessage = "El nombre no debe contener menos de 3 caracteres.")]
+        [MaxLength(50, ErrorMessage = "El nombre no debe contener más de 50 caracteres.")]
+
         public string nom_usuario { get; set; }
         [Display(Name = "Apellido de Usuario")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El apellido es obligatorio.")]
+        [MinLength(3, ErrorMessage = "El apellido no debe contener menos de 3 caracteres.")]
+        [MaxLength(50, ErrorMessage = "El apellido no debe contener más de 50 caracteres.")]
+
         public string ape_usuario { get; set; }
         [Display(Name = "Username")]
+        [Required]
         public string username { get; set; }
+       
+        [Required]
         public string pass { get; set; }
+
         [Display(Name = "E-mail")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El email es requerido.")]
+        [RegularExpression(@"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$", ErrorMessage
+          = "Formato incorrecto, coloca un correo valido.")]
+
         public string email { get; set; }
+
         [Display(Name = "Telefono")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "El telefono es requerido.")]
+       [RegularExpression("([0-9]+$)",ErrorMessage ="Solo se perimte números")]
         public string fono_user { get; set; }
+
         [Display(Name = "Rol")]
         public int id_rol { get; set; }
+
         [Display(Name = "Rol")]
         public string nom_rol { get; set; }
         [Display(Name = "Distrito")]
+
         public int id_distrito { get; set; }
+
         [Display(Name = "Distrito")]
         public string nom_distrito { get; set; }
         public int estado { get; set; }
