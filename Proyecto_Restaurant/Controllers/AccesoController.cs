1351206012a87bc9767ff360315a276a8d4ae13f
juliocruzmieses <juliocruzmieses@gmail.com>
2022-06-25 08:09:09 -0500

    alert_message

--- Proyecto_Restaurant/Controllers/AccesoController.cs
+++ Proyecto_Restaurant/Controllers/AccesoController.cs
@@ -28,24 +28,31 @@ namespace Proyecto_Restaurant.Controllers
             user.pass = ConvertirSha256(user.pass);
             using (SqlConnection cn=new SqlConnection(ConfigurationManager.ConnectionStrings["cadena"].ConnectionString))
             {
-                SqlCommand cmd = new SqlCommand("usp_ValidarUsuario", cn);
-                cmd.CommandType = CommandType.StoredProcedure;
-                cmd.Parameters.AddWithValue("username", user.username);
-                cmd.Parameters.AddWithValue("pass", user.pass);
-
-                cn.Open();
-                SqlDataReader dr = cmd.ExecuteReader();
-                while (dr.Read())
+                try
                 {
-                    user.id_usuario = dr.GetInt32(0);
-                    user.nom_usuario = dr.GetString(1);
-                    user.ape_usuario = dr.GetString(2);
-                    user.username = dr.GetString(3);
-                    user.email = dr.GetString(4);
-                    user.fono_user = dr.GetString(5);
-                    user.id_rolPermiso = (RolPermiso)dr.GetInt32(6);
-                    user.id_distrito = dr.GetInt32(7);
-                    user.nom_distrito = dr.GetString(8);
+                    SqlCommand cmd = new SqlCommand("usp_ValidarUsuario", cn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("username", user.username);
+                    cmd.Parameters.AddWithValue("pass", user.pass);
+
+                    cn.Open();
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    while (dr.Read())
+                    {
+                        user.id_usuario = dr.GetInt32(0);
+                        user.nom_usuario = dr.GetString(1);
+                        user.ape_usuario = dr.GetString(2);
+                        user.username = dr.GetString(3);
+                        user.email = dr.GetString(4);
+                        user.fono_user = dr.GetString(5);
+                        user.id_rolPermiso = (RolPermiso)dr.GetInt32(6);
+                        user.id_distrito = dr.GetInt32(7);
+                        user.nom_distrito = dr.GetString(8);
+                    }
+                }
+                catch(SqlException ex)
+                {
+                    ViewBag.error = ex.Message.ToString();
                 }
             }
             if (user.id_usuario!=0)
